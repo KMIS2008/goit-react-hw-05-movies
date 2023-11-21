243c8d35dfbdd2098242012048fa5a964fd24952
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2023-11-21 12:16:38 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+// import './index.css';
+
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    white: '#fafafa',
+    black: '#212121',
+    background: '#D3D3D3',
+    gray: "#808080",
+    blue: '#F0F8FF',
+    darkblue: '#4e5dda',
+    border: '#d8d1d1',
+    rose: '#f2ead1',
+    purpel: 'rgb(190 69 163)',
+    red: 'rgb(231 13 13)',
+    green: '#17b717',
+  },
+
+  spacing: value => `${value * 5}px`,
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
+  <React.StrictMode  basename="/goit-react-hw-05-movies">
+<ThemeProvider theme = {theme}>
     <App />
+</ThemeProvider>
   </React.StrictMode>
+
 );
+
+
