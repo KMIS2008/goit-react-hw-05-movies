710e95470b28563f461212a537c4a7570824c746
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2023-11-23 09:16:34 +0200

    home1

--- src/components/Pages/MovieDetails/MovieDetails.js
+++ src/components/Pages/MovieDetails/MovieDetails.js
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react"
+import { ToastContainer, toast } from 'react-toastify';
+import {fetchDitailsFilms} from '../../api';
+import Loader from 'components/Loader';
+import { useParams } from "react-router-dom";
+
+
 export default function MovieDetails(){
-    return{
-        
-    }
+    const params = useParams();
+
+    const[loading, setIsLoading]=useState();
+    const[ditail, setDitail]=useState();
+
+    useEffect(()=>{
+        const addDitailsFilm = async()=>{
+            try {
+                setIsLoading(true);
+                // setError(false);
+               
+                 const ditailFilm = await fetchDitailsFilms(params.movieId);
+                 setDitail(ditailFilm)
+              
+               } catch (error) {
+                toast.error('ERORR. Please try reloading this page!');
+                // setError(true);
+               } finally {
+                 setIsLoading(false);
+               }
+        }
+
+        addDitailsFilm();
+    },[params.movieId, setIsLoading, setDitail])
+
+    return(
+        <main>
+             {loading && (<Loader/>)}
+             <ToastContainer/>
+        </main>
+    )
 }
